d45b321eca10590f356bcf2d134e9ea435a49c44
Mitch Paiva <paivamit@grinnell.edu>
2024-09-07 21:30:02 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.io.PrintWriter;
 public class HelloWorld {
 public static void main (String[] args) {
   PrintWriter pen = new PrintWriter(System.out, true);
-  pen.println ("Hello, GitHub! Yay!");
+  pen.println ("Hello, GitHub! Yay! hahahah");
   pen.flush();
   pen.close();
 } // main(String[])
